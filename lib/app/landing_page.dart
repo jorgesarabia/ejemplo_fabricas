01da63c156b5ca7a28cf3342e212edd80b94bd23
Jorge Sarabia <jorge@sarabiajor.ge>
2021-06-10 22:03:07 -0300

    Placeholder para los estados del Login

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,11 +1,32 @@
 import 'package:ejemplo_fabricas/auth/presentation/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ejemplo_fabricas/auth/application/auth/auth_bloc.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const LoginPage();
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        return state.map(
+          initial: (_) => const _LocalSplash(),
+          isLoggedIn: (_) => Container(),
+          userIsNotLoggedIn: (_) => const LoginPage(),
+        );
+      },
+    );
+  }
+}
+
+class _LocalSplash extends StatelessWidget {
+  const _LocalSplash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:ejemplo_fabricas/app/injectable/injection.dart';
 import 'package:ejemplo_fabricas/app/landing_page.dart';
+import 'package:ejemplo_fabricas/auth/application/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   configureInjection();
@@ -10,12 +12,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ejemplo Fabricas',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => getIt<AuthBloc>()..add(const AuthEvent.appIsStarting()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Ejemplo Fabricas',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const LandingPage(),
       ),
-      home: const LandingPage(),
     );
   }
 }
